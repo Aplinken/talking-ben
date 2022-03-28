104db5a3ca4081135c85b6e4c5f3e269222d09ca
Aplinken <70909562+Aplinken@users.noreply.github.com>
2022-03-28 18:24:26 +0600

    patch 0.2

--- cogs/text.py
+++ cogs/text.py
@@ -1,4 +1,5 @@
 from turtle import color
+import sqlite3
 import discord
 from discord.ext import commands
 import random
@@ -11,10 +12,28 @@ class Text(commands.Cog):
 
     @commands.command()
     async def ben(self,ctx,q=None):
+        guild = ctx.guild.id
         if q is None:
             await ctx.send('Ben') #Bruh Ben
         else:
+            db = sqlite3.connect('data.sqlite')
+            cursor = db.cursor()
+            cursor.execute(f'''
+                SELECT * FROM main WHERE guild_id = {guild}
+            ''')
+            result = cursor.fetchone()
+            if result is None:
+                cursor.execute(f'''
+                    INSERT INTO main (guild_id, answered) VALUES ({guild},1)
+                ''')
+            else:     
+                cursor.execute(f'''
+                    UPDATE main SET answered = answered + 1 WHERE guild_id = {guild} 
+                ''')
+
             await ctx.send(random.choice(reply_list)) #Random Reply
+            db.commit()
+            cursor.close()
 
     @commands.command()
     async def help(self,ctx):
@@ -27,6 +46,20 @@ class Text(commands.Cog):
     @commands.command()
     async def ping(self,ctx):
         await ctx.send(f'My ping is: {round(ctx.bot.latency * 1000)}ms')
+
+    @commands.command()
+    async def ans(self,ctx):
+        guild = ctx.guild.id
+        db = sqlite3.connect('data.sqlite')
+        cursor = db.cursor()
+        cursor.execute(f'''
+            SELECT answered FROM main WHERE guild_id = {guild}
+        ''')
+        result = cursor.fetchone()
+        fm_result = str(result)
+
+        await ctx.send(f"Amount Of Questions Answered: {fm_result[1:-2]}")
+        
 
 
 def setup(client):
