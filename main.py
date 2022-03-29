8ea38172d584c7ea6fb6565d0c4832e618b394d8
AnotherTwinkle <79911438+AnotherTwinkle@users.noreply.github.com>
2022-03-30 00:28:07 +0600

    Switch to built-in `is_owner()` check for owner commands.

--- main.py
+++ main.py
@@ -2,50 +2,41 @@ import discord
 from discord.ext import commands
 import os
 
-ownerid = 742661420683886597
-
 # Client created
 intents = discord.Intents.default()
 intents.members = True
 client = commands.Bot(command_prefix=commands.when_mentioned_or('~'), case_insensitive=True)
 client.remove_command("help")
 
-#Starting Defaults
+# Starting Defaults
 
 @client.event
 async def on_ready():
 	await client.change_presence(activity=discord.Game(name="My Talking Angela"))
 
+@commands.is_owner()
 @client.command()
 async def load(ctx, extension):
-	if ctx.author.id == ownerid:
-		client.load_extension(f'cogs.{extension}')
-		await ctx.send(f'{extension} has been loaded!')
-	else:
-		return False
+	client.load_extension(f'cogs.{extension}')
+	await ctx.send(f'{extension} has been loaded!')
 
+@commands.is_owner()
 @client.command()
 async def unload(ctx, extension):
-	if ctx.author.id == ownerid:
-		client.unload_extension(f'cogs.{extension}')
-		await ctx.send(f'{extension} has been unloaded!')
-	else:
-		return False
+	client.unload_extension(f'cogs.{extension}')
+	await ctx.send(f'{extension} has been unloaded!')
 
+@commands.is_owner()
 @client.command()
 async def reload(ctx, extension):
-	if ctx.author.id == ownerid:
-		client.reload_extension(f'cogs.{extension}')
-		await ctx.send(f'{extension} has been reloaded!')
-	else:
-		return False
+	client.reload_extension(f'cogs.{extension}')
+	await ctx.send(f'{extension} has been reloaded!')
 
-#Finishing Touch
+# Finishing Touch
 
 for filename in os.listdir('./cogs'):
 	if filename.endswith('.py'):
 		client.load_extension(f'cogs.{filename[:-3]}')
 
 token = os.environ.get('TOKEN')
-	
-client.run(f'{token}')
+client.run(token)
