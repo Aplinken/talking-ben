15783801c6f9679dc4576a918d60dce62096cc37
Aplinken <70909562+Aplinken@users.noreply.github.com>
2022-03-30 18:08:00 +0600

    shit

--- cogs/text.py
+++ cogs/text.py
@@ -1,9 +1,29 @@
-import sqlite3
+import psycopg2
 import discord
 from discord.ext import commands
 import random
+import os
 
 reply_list = ['Yes.','No.','Hohoho','Ugh'] #List of possible replies
+
+def update_ans(guild):
+    db = psycopg2.connect(f"{os.environ.get('DATABASE')}")
+    cursor = db.cursor()
+    cursor.execute(f'''
+        SELECT * FROM main WHERE guild_id = {guild}
+    ''')
+    result = cursor.fetchone()
+    if result is None:
+        cursor.execute(f'''
+            INSERT INTO main (guild_id, answered) VALUES ({guild},1)
+        ''')
+    else:     
+        cursor.execute(f'''
+            UPDATE main SET answered = answered + 1 WHERE guild_id = {guild} 
+        ''')
+    db.commit()
+    cursor.close()    
+
 
 class Text(commands.Cog):
     def __init__(self,client):
@@ -15,25 +35,9 @@ class Text(commands.Cog):
         if q is None:
             await ctx.send('Ben') #Bruh Ben
         else:
-            db = sqlite3.connect('data.sqlite')
-            cursor = db.cursor()
-            cursor.execute(f'''
-                SELECT * FROM main WHERE guild_id = {guild}
-            ''')
-            result = cursor.fetchone()
-            if result is None:
-                cursor.execute(f'''
-                    INSERT INTO main (guild_id, answered) VALUES ({guild},1)
-                ''')
-            else:     
-                cursor.execute(f'''
-                    UPDATE main SET answered = answered + 1 WHERE guild_id = {guild} 
-                ''')
-
+            update_ans(guild=guild)
             await ctx.send(random.choice(reply_list)) #Random Reply
-            db.commit()
-            cursor.close()
-
+ 
     @commands.command()
     async def help(self,ctx):
         embed = discord.Embed(title='Help',color = discord.Colour.blue())
@@ -43,13 +47,17 @@ class Text(commands.Cog):
         await ctx.send(embed=embed)
 
     @commands.command()
+    async def source(self,ctx):
+        await ctx.send("https://github.com/Aplinken/talking-ben")
+
+    @commands.command()
     async def ping(self,ctx):
         await ctx.send(f'My ping is: {round(ctx.bot.latency * 1000)}ms')
 
     @commands.command()
     async def ans(self,ctx):
         guild = ctx.guild.id
-        db = sqlite3.connect('data.sqlite')
+        db = psycopg2.connect(f"{os.environ.get('DATABASE')}")
         cursor = db.cursor()
         cursor.execute(f'''
             SELECT answered FROM main WHERE guild_id = {guild}
--- cogs/voice.py
+++ cogs/voice.py
@@ -1,9 +1,11 @@
 import random
+import psycopg2
 import os
 from xmlrpc import client
 import discord
 from discord.ext import commands
 from discord import FFmpegPCMAudio
+from .text import update_ans
 
 vc_reply_list = ['Laugh.wav','Yes.wav','No.wav','Ugh.wav']
 
@@ -17,11 +19,15 @@ class Voice(commands.Cog):
 
         if voice is None: #JOIN VC   
             channel = ctx.author.voice.channel
-            await channel.connect()
+            if channel is None:
+                await ctx.send("You are not connected to any voice channels!")
+            else:
+                await channel.connect()
         else:
             reply = random.choice(vc_reply_list)
             print(reply)                                              
             voice.play(discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(f"./cogs/audio/{reply}")))
+            update_ans(guild=ctx.guild.id)
          
 def setup(client):
 	client.add_cog(Voice(client))
